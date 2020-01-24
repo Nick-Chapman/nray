d8903aef0b05e544da49da4cf8559caf4a0bf56b
Nick Chapman <nic@amadido.co.uk>
2020-01-24 20:22:13 +0000

    step 5, specular lighting; multiple light sources

--- src/Main.hs
+++ src/Main.hs
@@ -7,16 +7,16 @@ main = run
 run :: IO ()
 run = do
   let size = Size (1024,768)
+  --let size = Size (512,384)
   let _ = writeImage "six.ppm" sixPixelImage
   let _ = writeImage "gradient.ppm" $ gradientImage size
-  writeImage "scene.ppm" $ renderScene size scene1
+  writeImage "scene.ppm" $ renderScene size lighting1 scene1
 
 writeImage :: FilePath -> Image -> IO ()
 writeImage path image = do
   let ppm = show (quantizeImage image)
   putStrLn $ "writing: " <> path
   writeFile path ppm
-
 
 newtype Byte = Byte Int -- 0,255
 mkByte :: Int -> Byte
@@ -37,7 +37,6 @@ instance Show PPM where
 
 instance Show RGB where
   show (RGB (r,g,b)) = unwords [show r, show g, show b]
-
 
 newtype Intensity = Intensity Double -- 0..
 mkIntensity :: Double -> Intensity
@@ -68,7 +67,6 @@ quantizeIntensity (Intensity f) = mkByte n where
   n = min 255 $ truncate (f * 256)
 
 newtype Image = Image [[Col]]
-
 newtype Size = Size (Int,Int) -- width(x), height(y)
 
 sixPixelImage :: Image
@@ -86,17 +84,12 @@ gradientImage (Size (width,height)) =
   where
     float = fromIntegral
 
-
-newtype Scene = Scene [Surface]
-newtype Surface = Surface (Ray -> Maybe Hit)
-data Ray = Ray Point Direction
-data Hit = Hit { distance :: Double, material :: Material, hit :: Point, norm :: Direction }
-data Material = Material Col
-
-newtype Pixel = Pixel (Int,Int) -- i(x), j(y)
-newtype Norm = Norm Vec3f
-newtype Point = Point Vec3f
-newtype Direction = Direction Norm
+lighting1 :: Lighting
+lighting1 = Lighting
+  [ Light { position = mkPoint (-20, 20,  20), brightness = mkIntensity 1.5 }
+  , Light { position = mkPoint ( 30, 50, -25), brightness = mkIntensity 1.8 }
+  , Light { position = mkPoint ( 30, 20,  30), brightness = mkIntensity 1.7 }
+  ]
 
 scene1 :: Scene
 scene1 = Scene
@@ -107,15 +100,54 @@ scene1 = Scene
   ]
 
 ivory,redRubber :: Material
-ivory     = Material $ Col(mkIntensity 0.4, mkIntensity 0.4, mkIntensity 0.3)
-redRubber = Material $ Col(mkIntensity 0.3, mkIntensity 0.1, mkIntensity 0.1)
 
-renderScene :: Size -> Scene -> Image
-renderScene size@(Size (width,height)) scene =
+ivory = Material
+  { surfaceCol = Col(mkIntensity 0.4, mkIntensity 0.4, mkIntensity 0.3)
+  , diffAlbedo = mkIntensity 0.6
+  , specAlbedo = mkIntensity 0.3
+  , specExponent = 50
+  }
+
+redRubber = Material
+  { surfaceCol = Col(mkIntensity 0.3, mkIntensity 0.1, mkIntensity 0.1)
+  , diffAlbedo = mkIntensity 0.9
+  , specAlbedo = mkIntensity 0.1
+  , specExponent = 10
+  }
+
+newtype Lighting = Lighting [Light]
+data Light = Light { position :: Point, brightness :: Intensity }
+
+newtype Scene = Scene [Surface]
+newtype Surface = Surface (Ray -> Maybe Hit)
+data Ray = Ray Point Direction
+
+data Hit = Hit
+  { distance :: Double
+  , material :: Material
+  , hitPoint :: Point
+  , surfaceNorm :: Direction
+  }
+
+data Material = Material -- diff=diffuse, spec=specular
+  { surfaceCol :: Col
+  , diffAlbedo :: Intensity --0..1
+  , specAlbedo :: Intensity --0..1
+  , specExponent :: Double
+  }
+
+newtype Pixel = Pixel (Int,Int) -- i(x), j(y)
+newtype Norm = Norm Vec3f
+newtype Point = Point Vec3f
+newtype Direction = Direction Norm
+
+renderScene :: Size -> Lighting -> Scene -> Image
+renderScene size@(Size (width,height)) lighting scene =
   Image $ reverse $ [ [colourAtPixel (Pixel (i,j)) | i <- [0..width-1]] | j <- [0..height-1] ]
   where
     colourAtPixel :: Pixel -> Col
-    colourAtPixel pixel = renderMaybeHit $ castRayScene (eyeRay size pixel) scene
+    colourAtPixel pixel = renderMaybeHit lighting ray $ castRayScene ray scene where
+      ray = eyeRay size pixel
 
 castRayScene :: Ray -> Scene -> Maybe Hit
 castRayScene ray (Scene surfaces) = combineMaybeHits [ f ray | Surface f <- surfaces ]
@@ -129,34 +161,58 @@ combineMaybeHits mhs =
 combineHit :: Hit -> Hit -> Hit
 combineHit h1@Hit{distance=d1} h2@Hit{distance=d2} = if d1 < d2 then h1 else h2
 
-renderMaybeHit :: Maybe Hit -> Col
-renderMaybeHit = \case
+renderMaybeHit :: Lighting -> Ray -> Maybe Hit -> Col
+renderMaybeHit (Lighting lights) ray = \case
   Nothing -> bgColour
-  Just Hit {material = Material surfaceCol, hit = Point hitPoint, norm = Direction (Norm surfaceNorm)} -> do
-    let lightDir :: Vec3f = normalise (subVec lightPos hitPoint)
-    let lightIntensity :: Intensity = mkIntensity $ (brightness*) $ clamp $ dotProduct lightDir surfaceNorm
-    attenuateColour lightIntensity surfaceCol
-      where
-        clamp = max 0 --abs
-        lightPos = Vec3f (-20, 20, 20)
-        brightness = 1.5 -- causes intensity>1
+  Just Hit { material = Material {surfaceCol,diffAlbedo,specAlbedo,specExponent}
+           , hitPoint = Point hitPoint
+           , surfaceNorm = Direction (Norm surfaceNorm)
+           } ->
+    foldl1 addColour (map colourFromLight lights)
+    where
+      Ray _ (Direction (Norm lookDir)) = ray
+
+      colourFromLight :: Light -> Col
+      colourFromLight light = do
+        let Light { position = Point lightPos, brightness = Intensity brightness } = light
+        let lightDir = normalise (subVec lightPos hitPoint)
+        let diffComponent = clampPositve (dotProduct lightDir surfaceNorm)
+        let specComponent = clampPositve (dotProduct (reflect lightDir surfaceNorm) lookDir) ** specExponent
+        let diffLightIntensity = mkIntensity $ (brightness*) $ diffComponent
+        let specLightIntensity = mkIntensity $ (brightness*) $ specComponent
+        addColour
+          (attenuateColour (mulI diffLightIntensity diffAlbedo) surfaceCol)
+          (attenuateColour (mulI specLightIntensity specAlbedo) white)
+
+clampPositve :: Double -> Double
+clampPositve = max 0
+
+reflect :: Vec3f -> Vec3f -> Vec3f
+reflect incidence surfaceNorm =
+  subVec incidence (scaleVec ( 2 * dotProduct incidence surfaceNorm) surfaceNorm)
 
 attenuateColour :: Intensity -> Col -> Col
 attenuateColour a (Col (r,g,b)) = Col (mulI a r, mulI a g, mulI a b)
 
+addColour :: Col -> Col -> Col
+addColour (Col (r,g,b)) (Col (r',g',b')) = Col (addI r r', addI g g', addI b b')
+
 mulI :: Intensity -> Intensity -> Intensity
-mulI (Intensity a) (Intensity b) = mkIntensity (a*b) -- no need to check!?
+mulI (Intensity a) (Intensity b) = Intensity (a*b)
+
+addI :: Intensity -> Intensity -> Intensity
+addI (Intensity a) (Intensity b) = Intensity (a+b)
 
 bgColour :: Col
 bgColour = Col (mkIntensity 0.2, mkIntensity 0.7, mkIntensity 0.8)
 
 eyeRay :: Size -> Pixel -> Ray
 eyeRay (Size (width,height)) (Pixel (i,j)) = Ray origin direction where
-  origin = mkPoint (0,0,-4) --random hack. go a bit closer!
-  direction = mkDirection (x,y,z)
+  origin = mkPoint (0,0,0)
+  direction = mkDirection (x,y,z * 1.4) -- random hack to reduce FOV
   x = float $ i*2 - (width-1)
   y = float $ j*2 - (height-1)
-  z = float $ - (width-1) -- this give a 90 degree horizonal field of view
+  z = float $ - (width-1) -- this should give a 90 degree horizonal field of view
   float = fromIntegral
 
 mkPoint :: (Double,Double,Double) -> Point
@@ -177,9 +233,9 @@ normalise (Vec3f (x,y,z)) = Vec3f(x',y',z') where
 
 sphere :: Point -> Double -> Material -> Surface
 sphere (Point center) radius material = do
-  Surface $ \(Ray (Point orig) (Direction (Norm dir))) -> do
+  Surface $ \(Ray (Point orig) (Direction (Norm lookDir))) -> do
     let l :: Vec3f = subVec center orig
-    let tca :: Double = dotProduct l dir
+    let tca :: Double = dotProduct l lookDir
     let d2  :: Double = dotProduct l l - tca*tca
     let r2  :: Double = radius*radius
     if d2 > r2 then Nothing else do
@@ -189,9 +245,14 @@ sphere (Point center) radius material = do
       case if t0 < 0 then (if t1 < 0 then Nothing else Just t1) else Just t0
         of Nothing -> Nothing
            Just distance -> do
-             let hit = addVec orig (scaleVec distance dir)
-             let norm = mkNorm (subVec hit center)
-             Just Hit { distance, material, hit = Point hit, norm = Direction norm }
+             let hit = addVec orig (scaleVec distance lookDir)
+             let surfaceNorm = mkNorm (subVec hit center)
+             Just Hit
+               { distance
+               , material
+               , hitPoint = Point hit
+               , surfaceNorm = Direction surfaceNorm
+               }
 
 newtype Vec3f = Vec3f (Double,Double,Double) --xyz
 
