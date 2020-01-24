ddf6fcd33238f52f93066c1cdcec40468ebf61fa
Nick Chapman <nic@amadido.co.uk>
2020-01-24 17:21:49 +0000

    step 4, lighting

--- src/Main.hs
+++ src/Main.hs
@@ -97,7 +97,7 @@ gradientImage (Size (width,height)) =
 newtype Scene = Scene [Surface]
 newtype Surface = Surface (Ray -> Maybe Hit)
 data Ray = Ray Point Direction
-data Hit = Hit { distance :: Double, material :: Material }
+data Hit = Hit { distance :: Double, material :: Material, hit :: Point, norm :: Direction }
 data Material = Material Col
 
 newtype Pixel = Pixel (Int,Int) -- i(x), j(y)
@@ -133,20 +133,32 @@ combineMaybeHits mhs =
     [] -> Nothing
     hits -> Just $ foldl1 combineHit hits
 
-combineHit :: Hit -> Hit -> Hit -- This will get more complicated when we need to know which surface we hit
+combineHit :: Hit -> Hit -> Hit
 combineHit h1@Hit{distance=d1} h2@Hit{distance=d2} = if d1 < d2 then h1 else h2
 
 renderMaybeHit :: Maybe Hit -> Col
 renderMaybeHit = \case
-  Just Hit {material = Material col} -> col
   Nothing -> bgColour
+  Just Hit {material = Material surfaceCol, hit = Point hitPoint, norm = Direction (Norm surfaceNorm)} -> do
+    let lightDir :: Vec3f = normalise (subVec lightPos hitPoint)
+    let lighIntensity :: Intensity = mkIntensity $ (brightness*) $ abs $ dotProduct lightDir surfaceNorm
+    attenuateColour lighIntensity surfaceCol
+      where
+        lightPos = Vec3f (-20, 20, 20)
+        brightness = 1.0 --1.5 (this can cause intensity>1)
+
+attenuateColour :: Intensity -> Col -> Col
+attenuateColour a (Col (r,g,b)) = Col (mulI a r, mulI a g, mulI a b)
+
+mulI :: Intensity -> Intensity -> Intensity
+mulI (Intensity a) (Intensity b) = mkIntensity (a*b) -- no need to check!?
 
 bgColour :: Col
 bgColour = Col (mkIntensity 0.2, mkIntensity 0.7, mkIntensity 0.8)
 
 eyeRay :: Size -> Pixel -> Ray
 eyeRay (Size (width,height)) (Pixel (i,j)) = Ray origin direction where
-  origin = mkPoint (0,0,0)
+  origin = mkPoint (0,0,-4) --random hack. go a bit closer!
   direction = mkDirection (x,y,z)
   x = float $ i*2 - (width-1)
   y = float $ j*2 - (height-1)
@@ -180,9 +192,12 @@ sphere (Point center) radius material = do
       let thc :: Double = sqrt (r2-d2)
       let t0  :: Double = tca - thc
       let t1  :: Double = tca + thc
-      if t0 < 0
-        then if t1 < 0 then Nothing else Just Hit { distance = t0, material }
-        else Just Hit { distance = t0, material }
+      case if t0 < 0 then (if t1 < 0 then Nothing else Just t1) else Just t0
+        of Nothing -> Nothing
+           Just distance -> do
+             let hit = addVec orig (scaleVec distance dir)
+             let norm = mkNorm (subVec hit center)
+             Just Hit { distance, material, hit = Point hit, norm = Direction norm }
 
 newtype Vec3f = Vec3f (Double,Double,Double) --xyz
 
@@ -191,3 +206,9 @@ dotProduct (Vec3f (x,y,z)) (Vec3f (x',y',z')) = x*x' + y*y' + z*z'
 
 subVec :: Vec3f -> Vec3f -> Vec3f
 subVec (Vec3f (x,y,z)) (Vec3f (x',y',z')) = Vec3f (x-x',y-y',z-z')
+
+addVec :: Vec3f -> Vec3f -> Vec3f
+addVec (Vec3f (x,y,z)) (Vec3f (x',y',z')) = Vec3f (x+x',y+y',z+z')
+
+scaleVec :: Double -> Vec3f -> Vec3f
+scaleVec m (Vec3f (x,y,z)) = Vec3f (m*x,m*y,m*z)
