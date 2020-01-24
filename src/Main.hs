61753b3c438c6269e573ff8818e853b4d14a60ff
Nick Chapman <nic@amadido.co.uk>
2020-01-24 15:17:37 +0000

    step2, one sphere

--- src/Main.hs
+++ src/Main.hs
@@ -16,6 +16,7 @@ run = do
   --let size = Size (300,200)
   writeImage "six.ppm" sixPixelImage
   writeImage "gradient.ppm" $ gradientImage size
+  writeImage "step2.ppm" $ renderScene size scene1
 
 writeImage :: FilePath -> Image -> IO ()
 writeImage path image = do
@@ -91,3 +92,93 @@ gradientImage (Size (width,height)) =
           ]
   where
     float = fromIntegral
+
+
+newtype Scene = Scene [Surface]
+newtype Surface = Surface (Ray -> Maybe Hit)
+data Ray = Ray Point Direction
+data Hit = Hit -- here we will add things like the distance to hit & the normal. but for now it just unit
+
+newtype Pixel = Pixel (Int,Int) -- i(x), j(y)
+newtype Norm = Norm Vec3f
+newtype Point = Point Vec3f
+newtype Direction = Direction Norm
+
+scene1 :: Scene
+scene1 = Scene [ sphere (mkPoint (-3,0,-16)) 2 ]
+
+renderScene :: Size -> Scene -> Image
+renderScene size@(Size (width,height)) scene =
+  Image $ [ [colourAtPixel (Pixel (i,j)) | i <- [0..width-1]] | j <- [0..height-1] ]
+  where
+    colourAtPixel :: Pixel -> Col
+    colourAtPixel pixel = renderMaybeHit $ castRayScene (eyeRay size pixel) scene
+
+castRayScene :: Ray -> Scene -> Maybe Hit
+castRayScene ray (Scene surfaces) = combineMaybeHits [ f ray | Surface f <- surfaces ]
+
+combineMaybeHits :: [Maybe Hit] -> Maybe Hit
+combineMaybeHits mhs =
+  case [ h | Just h <- mhs ] of
+    [] -> Nothing
+    hits -> Just $ foldl1 combineHit hits
+
+combineHit :: Hit -> Hit -> Hit -- This will get more complicated when we need to know which surface we hit
+combineHit Hit Hit = Hit
+
+renderMaybeHit :: Maybe Hit -> Col
+renderMaybeHit = \case
+  Just _ -> obColour
+  Nothing -> bgColour
+
+bgColour,obColour :: Col
+obColour = Col (mkIntensity 0.4, mkIntensity 0.4, mkIntensity 0.3)
+bgColour = Col (mkIntensity 0.2, mkIntensity 0.7, mkIntensity 0.8)
+
+eyeRay :: Size -> Pixel -> Ray
+eyeRay (Size (width,height)) (Pixel (i,j)) = Ray origin direction where
+  origin = mkPoint (0,0,0)
+  direction = mkDirection (x,y,z)
+  x = float $ i*2 - (width-1)
+  y = float $ j*2 - (height-1)
+  z = float $ - (width-1) -- this give a 90 degree horizonal field of view
+  float = fromIntegral
+
+mkPoint :: (Double,Double,Double) -> Point
+mkPoint xyz = Point (Vec3f xyz)
+
+mkDirection :: (Double,Double,Double) -> Direction
+mkDirection xyz = Direction (mkNorm (Vec3f xyz))
+
+mkNorm :: Vec3f -> Norm
+mkNorm vec = Norm (normalise vec)
+
+normalise :: Vec3f -> Vec3f
+normalise (Vec3f (x,y,z)) = Vec3f(x',y',z') where
+  x' = x / len
+  y' = y / len
+  z' = z / len
+  len = sqrt (x*x + y*y + z*z)
+
+sphere :: Point -> Double -> Surface
+sphere (Point center) radius = do
+  Surface $ \(Ray (Point orig) (Direction (Norm dir))) -> do
+    let l :: Vec3f = subVec center orig
+    let tca :: Double = dotProduct l dir
+    let d2  :: Double = dotProduct l l - tca*tca
+    let r2  :: Double = radius*radius
+    if d2 > r2 then Nothing else do
+      let thc :: Double = sqrt (r2-d2)
+      let t0  :: Double = tca - thc
+      let t1  :: Double = tca + thc
+      if t0 < 0
+        then if t1 < 0 then Nothing else Just Hit
+        else Just Hit
+
+newtype Vec3f = Vec3f (Double,Double,Double) --xyz
+
+dotProduct :: Vec3f -> Vec3f -> Double
+dotProduct (Vec3f (x,y,z)) (Vec3f (x',y',z')) = x*x' + y*y' + z*z'
+
+subVec :: Vec3f -> Vec3f -> Vec3f
+subVec (Vec3f (x,y,z)) (Vec3f (x',y',z')) = Vec3f (x-x',y-y',z-z')
